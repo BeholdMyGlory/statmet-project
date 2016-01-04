da52c66b4820b652b4013ee03fd8f03277adb6d8
JayTheBestMan <jaythebestman@hotmail.com>
2016-01-04 13:17:56 +0100

    Uppdatering
    Förhoppnigsvis har jag lagt till en mer eller mindre fungerande
    simulering av ett tåg som åker. Man får både observationer och den
    faktiska stigen.

--- generate_g.py
+++ generate_g.py
@@ -1,6 +1,7 @@
 import numpy as np;
 import random;
 
+# Genererar grannmatris. Använd inte om vi inte måste
 def generate_graph_1(n):
         G = np.zeros((n, n));
         degree = np.zeros(n);
@@ -22,7 +23,8 @@ def generate_graph_1(n):
                                         print('Failed to generate graph');
                                         return np.zeros(1);
         return G;
-	
+
+# Genererar grannlistor.	
 def generate_graph_2(n):
         G = np.ones((n, 3))*(-1);
         degree = np.zeros(n);
@@ -46,6 +48,7 @@ def generate_graph_2(n):
                                         return np.zeros(1);
         return G;
 
+# Genererar labels för switcharna. 1 = 0, 2 = L, 3 = R
 def generate_labels_randomly(n):
         l = np.zeros((n, 3));
         for a in range(0,3):
@@ -58,16 +61,55 @@ def generate_labels_randomly(n):
                                         break;
         return l;
 
+# Genererar sigma. 0 = L, 1 = R
 def generate_settings_randomly(n):
         sig = np.zeros(n);
         for i in range(0, n):
                 sig[i] = random.randrange(0,2);
         return sig;
 
+# Genererar graf och inställningar.
 def generate_graph_and_settings(n):
         G = np.zeros(1);
         while G.size == 1:
                 G = generate_graph_2(n);
         l = generate_labels_randomly(n);
         sig = generate_settings_randomly(n);
-        return (G, l, sig);
+        return (n, G, l, sig);
+
+# Simulerar tåg med given graf
+def simulate_train(graph, t):
+        O = [];
+        actual_path = [];
+        s = random.randrange(0,int(graph[0]));
+        orientation = random.randrange(1,4);
+        actual_path.append(s);
+        
+        for i in range(0, t-1):
+                # Hitta nästa håll att åka mot
+                # Samt gör observation av hur man åker
+                # OL = 1, OR = 2, L0 = 3, R0 = 4
+                ext = 0;
+                obs = 0;
+                if orientation == 1:
+                        if graph[3][s] == 0:
+                                obs = 1;
+                                ext = 2;
+                        else:
+                                obs = 2;
+                                ext = 3;
+                else:
+                        ext = 1;
+                        if orientation == 2:
+                                obs = 3;
+                        else:
+                                obs = 4;
+                O.append(obs);
+                        
+                for a in range(0,3):
+                        if graph[2][s,a] == ext:
+                                s = graph[1][s,a];
+                                actual_path.append(s);
+                                orientation = graph[2][s,a];
+                                break;
+        return (O, actual_path);
